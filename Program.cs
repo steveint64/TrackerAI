89355cfcff05bbee68cb65633c5ef32cc720ac7f
steveint64 <steve.int64@live.com>
2025-04-23 14:45:07 -0700

    🚀 Fresh clean start for TrackerAI.io - no secrets, just greatness

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TrackerAI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.Run();
+        }
+    }
+}
