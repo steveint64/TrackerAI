05c8a5060db8d9ceb9697cd0a1ff9dd45869e35f
steveint64 <steve.int64@live.com>
2025-05-25 13:11:53 -0700

    🔥 Removed Razor Pages, now serving static site from wwwroot

--- Program.cs
+++ Program.cs
@@ -1,34 +1,8 @@
-namespace TrackerAI
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-            // Add services to the container.
-            builder.Services.AddRazorPages();
+// Serve default files (like index.html) and other static content in wwwroot
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapRazorPages();
-
-            app.Run();
-        }
-    }
-}
+app.Run();
