0a03b81423e8b3d576eaace4c766ccc8e9878714
steveint64 <steve.int64@live.com>
2025-05-26 13:35:14 -0700

    Trigger deploy for trackerai.io

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-// Serve default files (like index.html) and other static content in wwwroot
-app.UseDefaultFiles();
-app.UseStaticFiles();
+// Serve wwwroot files (like /index.html)
+app.UseDefaultFiles();  // Looks for index.html
+app.UseStaticFiles();   // Serves it
 
 app.Run();
